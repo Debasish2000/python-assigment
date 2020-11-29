bf30b19132d9b7aac6949d126bc7646a07eaf17e
Debasish2000 <75217922+Debasish2000@users.noreply.github.com>
2020-11-29 23:40:00 +0530

    Add files via upload

---
+++ fileextension.py
@@ -0,0 +1,34 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.py
+The extension of the file is : 'py'
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.py
+Traceback (most recent call last):
+  File "C:/Users/Debasish Thaosen/Desktop/fileextensio.py", line 3, in <module>
+    print("The extension of the file is : "+repr(f_extensions[-1]))
+NameError: name 'f_extensions' is not defined
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.py
+The extension of the file is : 'py'
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.py
+The extension of the file is : 'py'
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.java
+The extension of the file is : 'java'
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.phy
+The extension of the file is : 'phy'
+>>> 
+========== RESTART: C:/Users/Debasish Thaosen/Desktop/fileextensio.py ==========
+Input the Filename:abc.python
+The extension of the file is : 'python'
+>>> 
